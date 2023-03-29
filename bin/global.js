5b10430a190b668863e324b72276c5ad140df0fb
David Trujillo <davot098@gmail.com>
2023-03-29 00:50:57 -0500

    Primer commit

---
+++ bin/global.js
@@ -0,0 +1,5 @@
+let triangle = require ('../src/index.js');
+
+const rndInt = Math.floor(Math.random() * 10) + 1;
+
+triangle.seeTriangles(rndInt);
---
+++ src/index.js
@@ -0,0 +1,18 @@
+function seeTriangles(n){
+    let aux = n;
+    for (let i = 0; i < n; i++) {
+        let triangle = '';
+        for (let j = aux; j > 0; j--) {
+            triangle += ' ';
+        }
+        for (let j = 0; j < (2*i-1); j++) {
+            triangle += '*';
+        }
+        aux--;
+        console.log(triangle);  
+    }
+}
+
+module.exports = {
+    seeTriangles
+}
